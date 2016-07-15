9f53c44a19f301a4ef3912a0af503ecd62ff0d94
Avi Stramer <astramer@gmail.com>
2016-07-16 00:13:39 +0100

    add engine.io server support; more consistent echo service

--- app/index.js
+++ app/index.js
@@ -15,15 +15,29 @@ logger.add(winston.transports.RollingFile, { filename: logFile, level: logLevel,
 var io = require('socket.io')(config.get("Service.port"));
 
 io.on('connection', function (socket) {
-    logger.info('Received new connection: ', socket.id);
+    logger.info('Received new socket.io connection: ', socket.id);
 
-    socket.on('message', function (msg) {
+    socket.on('event', function (msg) {
         logger.verbose('Received msg ', msg, ' from ', socket.id);
         socket.emit('event', 'Echo: ' + msg);
     });
 
     socket.on('disconnect', function () {
-        logger.info('Disconnected: ', socket.id)
-        socket.emit('user disconnected');
+        logger.info('Disconnected: ', socket.id);
+    });
+});
+
+var engine = require('engine.io');
+var server = engine.listen(config.get("Service.port") + 1);
+
+server.on('connection', function (socket) {
+    logger.info('Received new engine.io connection: ', socket.id);
+    socket.on('message', function (msg) {
+        logger.verbose('Received msg ', msg, ' from ', socket.id);
+        socket.send('Echo: ' + msg);
+    });
+
+    socket.on('close', function () {
+        logger.info('Disconnected: ', socket.id);
     });
 });
